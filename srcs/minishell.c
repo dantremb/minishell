1549959177347577f69dbcb7c9dd761b46b8b3e2
dantremb <dany.tremblay@gmail.com>
2022-09-10 20:34:04 -0400

    10 September 20:34:04

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -13,16 +13,17 @@
 #include "../includes/minishell.h"
 t_data data;
 
-/* ************************MINISHELL UTILS************************************ */
+/* **********************MINISHELL UTILS************************************* */
 
-void ft_free_table(void)
+void	ft_free_table(void)
 {
-	int i;
+	int	i;
 
 	i = -1;
 	while (++i < data.cmd_count)
 		free(data.cmd[i].token);
 	free(data.cmd);
+	free(data.pid);
 	free(data.buffer);
 }
 
@@ -33,67 +34,46 @@ void	ft_exit(char *str, int s)
 	if (s <= 1)
 		free(data.buffer);
 	if (s <= 2)
+	{
 		ft_free_array(data.env);
+		free(data.pid);
+	}
 	if (s <= 3)
 		ft_free_table();
 	exit(0);
 }
 
-void	ft_init_environement(char **env)
-{
-	int		i;
-
-	data.expand[0] = 'a';
-	data.expand[1] = '\0';
-	data.heredoc[0] = 'a';
-	data.heredoc[1] = '\0';
-	data.buffer = NULL;
-	data.prompt = NULL;
-	data.cmd_count = 0;
-	data.cmd = NULL;
-	i = 0;
-	data.env = ft_calloc(sizeof(char *), ft_array_size(env));
-	if (data.env == NULL)
-		ft_exit("Malloc Error\n", 0);
-	while (env[i])
-	{
-		data.env[i] = ft_strdup(env[i]);
-		if (!data.env[i])
-			ft_exit("Malloc Error\n", 0);
-		i++;
-	}
-}
-
 void	ft_print_table(void)
 {
-	int i;
-	int j;
-	
+	int	i;
+	int	j;
+
 	i = 0;
-	ft_color(RED);
-	printf("---------- COMAND TABLE ----------\n");
+	ft_color(1);
+	dprintf(2, "---------- COMAND TABLE ----------\n");
 	while (i < data.cmd_count)
 	{
 		j = 0;
-		ft_color(CYAN);
-		printf("[cmd %d]", i);
+		ft_color(6);
+		dprintf(2, "[cmd %d]", i);
 		while (data.cmd[i].token[j])
 		{
-			ft_color(YELLOW);
-			printf("[%s]", data.cmd[i].token[j]);
+			ft_color(3);
+			dprintf(2, "[%s]", data.cmd[i].token[j]);
 			j++;
 		}
-		printf("\n");
+		dprintf(2, "\n");
 		i++;
 	}
-	ft_color(RED);
-	printf("---------------END----------------\n");
-	ft_color(WHITE);
+	ft_color(1);
+	dprintf(2, "---------------END----------------\n");
+	ft_color(0);
 }
 
 char	*ft_get_variable(char *buffer)
 {
 	int		i;
+
 	i = -1;
 	while (data.env[++i])
 	{
@@ -111,11 +91,11 @@ char	*ft_get_prompt(void)
 {
 	char	*prompt;
 
-	prompt = ft_strjoin("\033[31;36m", ft_get_variable("USER") , 0);
+	prompt = ft_strjoin("\033[0;32m", ft_get_variable("USER"), 0);
 	prompt = ft_strjoin(prompt, "@", 1);
 	prompt = ft_strjoin(prompt, "Minishell", 1);
 	prompt = ft_strjoin(prompt, ": ", 1);
-	prompt = ft_strjoin(prompt, "\033[30;104m", 1);
+	prompt = ft_strjoin(prompt, "\033[0;34m", 1);
 	prompt = ft_strjoin(prompt, ft_get_variable("PWD"), 1);
 	prompt = ft_strjoin(prompt, "> ", 1);
 	prompt = ft_strjoin(prompt, "\033[0m", 1);
@@ -143,39 +123,113 @@ bool	ft_check_builtin(int nb, int i)
 	return (true);
 }
 
-/* ******************BUILT-IN************************************************ */
-
-void	ft_cd(char *buffer)
+int	ft_open_fd(char *str, int i)
 {
-	int new;
-	int old;
-	int ret;
+	static int	fd;
 
-	ret = chdir(buffer);
-	old = -1;
-	while (data.env[++old])
-		if (ft_strncmp(data.env[old], "OLDPWD=", 7) == 0)
-			break ;
-	new = -1;
-	while (data.env[++new])
-		if (ft_strncmp(data.env[new], "PWD=", 4) == 0)
-			break ;
-	if (ret == 0)
+	if (i == 1)
+		fd = open(str, O_RDONLY, 0644);
+	else if (i == 2)
+		fd = open(str, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else if (i == 3)
+		fd = open(str, O_RDWR | O_CREAT | O_TRUNC, 0644);
+	else if (i == 4)
+		fd = open(str, O_RDWR | O_CREAT, 0644);
+	else if (i == 5)
+		fd = open(str, O_WRONLY | O_CREAT, 0644);
+	if (!fd)
+		return (-1);
+	return (fd);
+}
+
+void	ft_init_environement(char **env)
+{
+	int i;
+
+	data.expand[0] = 'a';
+	data.expand[1] = '\0';
+	data.heredoc[0] = 'a';
+	data.heredoc[1] = '\0';
+	data.buffer = NULL;
+	data.prompt = NULL;
+	data.cmd_count = 0;
+	data.cmd = NULL;
+	i = 0;
+	data.env = ft_calloc(sizeof(char *), ft_array_size(env));
+	if (data.env == NULL)
+		ft_exit("Malloc Error\n", 0);
+	while (env[i])
 	{
-		free (data.env[old]);
-		data.env[old] = ft_strjoin("OLDPWD=", data.env[new] + 4, 0);
-		free (data.env[new]);
-		data.env[new] = ft_strjoin("PWD=", getcwd(NULL, 0), 0);
+		data.env[i] = ft_strdup(env[i]);
+		if (!data.env[i])
+			ft_exit("Malloc Error\n", 0);
+		i++;
 	}
+}
+
+/* **********************BUILT-IN******************************************** */
+
+void	ft_echo(char **arg)
+{
+	int	i;
+	int	flag;
+
+	flag = 0;
+	i = 1;
+	if (arg[1] && ft_strncmp(arg[1], "-n\0", 3) == 0)
+	{
+		flag = 1;
+		i++;
+	}
+	while (arg[i])
+	{
+		if (ft_is_only(arg[i], ' '))
+			i++;
+		else
+		{
+			printf("%s", arg[i++]);
+			if (arg[i])
+				printf(" ");
+		}
+	}
+	if (flag == 0)
+		printf("\n");
 }
 
 void	ft_env(void)
 {
-	int i;
+	int	i;
 
 	i = -1;
-	while(data.env[++i])
+	while (data.env[++i])
 		printf("%s\n", data.env[i]);
+}
+
+void	ft_cd(char *buffer)
+{
+	int	new;
+	int	old;
+	int	ret;
+
+	ret = chdir(buffer);
+	old = -1;
+	while (data.env[++old])
+	{
+		if (ft_strncmp(data.env[old], "OLDPWD=", 7) == 0)
+			break ;
+	}
+	new = -1;
+	while (data.env[++new])
+	{
+		if (ft_strncmp(data.env[new], "PWD=", 4) == 0)
+			break ;
+	}
+	if (ret == 0)
+	{
+		free (data.env[old]);
+		data.env[old] = data.env[new];
+		data.env[new] = ft_strjoin("PWD=", getcwd(NULL, 0), 0);
+	}
 }
 
 void	ft_unset(char *buffer)
@@ -199,34 +253,7 @@ void	ft_unset(char *buffer)
 	}
 }
 
-void	ft_echo(char **arg)
-{
-	int i;
-	int flag;
-
-	flag = 0;
-	i = 1;
-	if (arg[1] && ft_strncmp(arg[1], "-n\0", 3) == 0)
-	{
-		flag = 1;
-		i++;
-	}
-	while (arg[i])
-	{
-		if (ft_is_only(arg[i], ' '))
-			i++;
-		else
-		{
-		printf("%s", arg[i++]);
-		if (arg[i])
-			printf(" ");
-		}
-	}
-	if (flag == 0)
-		printf("\n");
-}
-
-void ft_export(char *arg)
+void	ft_export(char *arg)
 {
 	char	**temp;
 	char	*duplicate;
@@ -252,10 +279,139 @@ void ft_export(char *arg)
 	}
 	else if (arg == NULL)
 		ft_env();
-
 }
 
-/* **************************PARSING***************************************** */
+void	ft_heredoc(char *limiter, char *heredoc)
+{
+	char	*str;
+	int		fd;
+	int		i;
+
+	fd = ft_open_fd(heredoc, 2);
+	if (fd == -1)
+	{
+		printf("Error: heredoc file could not be opened\n");
+		return ;
+	}
+	str = readline("<heredoc> ");
+	i = 1;
+	while (str)
+	{
+		if (ft_strnstr(str, limiter, ft_strlen(limiter)) != 0
+			&& str[ft_strlen(limiter)] == '\0')
+			break ;
+		else if (i != 1)
+			ft_putstr_fd("\n", fd);
+		ft_putstr_fd(str, fd);
+		free(str);
+		str = readline("<heredoc> ");
+		i = 0;
+	}
+	close(fd);
+	free(str);
+}
+
+/* **********************PARSING********************************************* */
+
+char	*ft_remove_char(char *token, char sep)
+{
+	int i;
+	int j;
+	
+	i = 0;
+	j = 0;
+	while (token[i])
+	{
+		if (token[i] == sep)
+		{
+			j = i - 1;
+			while (token[++j])
+				token[j] = token[j + 1];
+		}
+		else
+			i++;
+	}
+	return (token);
+}
+
+char	*ft_get_path(int nb)
+{
+	char	*program;
+	char	*env_path;
+	char	**fcnt_path;
+	char 	*test_path;
+	int		i;
+	i = -1;
+	if (access(data.cmd[nb].token[0], F_OK | X_OK) == 0)
+		return (data.cmd[nb].token[0]);
+	program = ft_strjoin("/", data.cmd[nb].token[0], 0);
+	env_path = ft_get_variable("PATH");
+	fcnt_path = ft_split(env_path, ':');
+	if (program == NULL || env_path == NULL || fcnt_path == NULL)
+		return (NULL);
+	while (fcnt_path[++i])
+	{
+		test_path = ft_strjoin(fcnt_path[i], program, 0);
+		if (access(test_path, F_OK | X_OK) == 0)
+			break ;
+		free (test_path);
+		test_path = NULL;
+	}
+	ft_free_array(fcnt_path);
+	free(program);
+	return (test_path);
+}
+
+char	*ft_expand(char *token, int flag)
+{
+	char	*temp[4];
+
+	temp[0] = ft_strchr(token, '$');
+	temp[1] = ft_remove_char(ft_substr(token, 0, temp[0] - token), '\"');
+	temp[2] = temp[0] + 1;
+	while (++temp[0])
+		if (*temp[0] == '\0' || *temp[0] == ' ' || *temp[0] == '$'
+			|| *temp[0] == '"' || *temp[0] == '\'')
+			break ;
+	temp[2] = ft_substr(temp[2] , 0, temp[0] - temp[2] );
+	temp[3] = ft_get_variable(temp[2] );
+	temp[3]  = ft_strjoin(temp[1], temp[3], 1);
+	free (temp[2]);
+	temp[1] = ft_remove_char(ft_substr(temp[0], 0, ft_strlen(temp[0])), '\"');
+	temp[0] = ft_strjoin(temp[3] , temp[1], 1);
+	free(temp[1]);
+	if (flag == 1)
+		free (token);
+	if (ft_strchr(temp[0], '$'))
+		temp[0] = ft_expand(temp[0], 1);
+	return (temp[0]);
+}
+
+char	*ft_expand_variable(char *token)
+{
+	char	*temps;
+	char	*expand;
+
+	if (token[0] == '$' && ft_strchr(&token[1], '$') == NULL)
+		token = ft_get_variable(&token[1]);
+	else
+	{
+		if (token[0] == '\"')
+			token = ft_expand(token + 1, 0);
+		else
+			token = ft_expand(token, 0);
+		expand = ft_strjoin(data.expand, "-expand=", 0);
+		temps = ft_strjoin(expand, token, 0);
+		free(token);
+		ft_export(temps);
+		free(temps);
+		expand[ft_strlen(expand) - 1] = '\0';
+		token = ft_get_variable(expand);
+		free(expand);
+		data.expand[0] = data.expand[0] + 1; 
+	}
+	return (token);
+}
 
 char	*ft_trim_token(char *buffer, char sep)
 {
@@ -291,100 +447,6 @@ int	ft_token_count(char *buffer, char sep)
 	return (i);
 }
 
-char	*ft_remove_char(char *token, char sep)
-{
-	int i;
-	int j;
-	
-	i = 0;
-	j = 0;
-	while (token[i])
-	{
-		if (token[i] == sep)
-		{
-			j = i;
-			while (token[j])
-			{
-				token[j] = token[j + 1];
-				j++;
-			}
-		}
-		else
-			i++;
-	}
-	return (token);
-}
-
-int	ft_open_fd(char *str, int i)
-{
-	static int	fd;
-
-	if (i == 1)
-		fd = open(str, O_RDONLY, 0644);
-	else if (i == 2)
-		fd = open(str, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	else if (i == 3)
-		fd = open(str, O_RDWR | O_CREAT | O_TRUNC, 0644);
-	else if (i == 4)
-		fd = open(str, O_RDWR | O_CREAT, 0644);
-	else if (i == 5)
-		fd = open(str, O_WRONLY | O_CREAT, 0644);
-	if (!fd)
-		return (-1);
-	return (fd);
-}
-
-char	*ft_expand_token(char *token, int flag)
-{
-	char	*temp[4];
-
-	temp[0] = ft_strchr(token, '$');
-	temp[1] = ft_remove_char(ft_substr(token, 0, temp[0] - token), '\"');
-	temp[2] = temp[0] + 1;
-	while (++temp[0])
-		if (*temp[0] == '\0' || *temp[0] == ' ' || *temp[0] == '$' || *temp[0] == '"' || *temp[0] == '\'')
-			break ;
-	temp[2] = ft_substr(temp[2] , 0, temp[0] - temp[2] );
-	temp[3] = ft_get_variable(temp[2] );
-	temp[3]  = ft_strjoin(temp[1], temp[3], 1);
-	free (temp[2]);
-	temp[1] = ft_remove_char(ft_substr(temp[0], 0, ft_strlen(temp[0])), '\"');
-	temp[0] = ft_strjoin(temp[3] , temp[1], 1);
-	free(temp[1]);
-	if (flag == 1)
-		free (token);
-	if (ft_strchr(temp[0], '$'))
-		temp[0] = ft_expand_token(temp[0], 1);
-	return (temp[0]);
-}
-
-
-char	*ft_expand_variable(char *token)
-{
-	char	*temps;
-	char	*expand;
-
-	if (token[0] == '$' && ft_strchr(&token[1], '$') == NULL)
-		token = ft_get_variable(&token[1]);
-	else
-	{
-		if (token[0] == '\"')
-			token = ft_expand_token(token + 1, 0);
-		else
-			token = ft_expand_token(token, 0);
-		expand = ft_strjoin(data.expand, "-expand=", 0);
-		temps = ft_strjoin(expand, token, 0);
-		free(token);
-		ft_export(temps);
-		free(temps);
-		expand[ft_strlen(expand) - 1] = '\0';
-		token = ft_get_variable(expand);
-		free(expand);
-		data.expand[0] = data.expand[0] + 1; 
-	}
-	return (token);
-}
-
 void	ft_clean_token(char **token)
 {
 	int t;
@@ -409,36 +471,6 @@ void	ft_clean_token(char **token)
 		}
 		t++;
 	}
-}
-
-void	ft_heredoc(char *limiter, char *heredoc)
-{
-	char	*str;
-	int		fd;
-	int		i;
-
-	fd = ft_open_fd(heredoc, 2);
-	if (fd == -1)
-	{
-		printf("Error: heredoc file could not be opened\n");
-		return ;
-	}
-	str = readline("<heredoc> ");
-	i = 1;
-	while (str)
-	{
-		if (ft_strnstr(str, limiter, ft_strlen(limiter)) != 0
-			&& str[ft_strlen(limiter)] == '\0')
-			break ;
-		else if (i != 1)
-			ft_putstr_fd("\n", fd);
-		ft_putstr_fd(str, fd);
-		free(str);
-		str = readline("<heredoc> ");
-		i = 0;
-	}
-	close(fd);
-	free(str);
 }
 
 char	*ft_expand_heredoc(char *heredoc)
@@ -504,6 +536,12 @@ void	ft_make_token(void)
 	c = -1;
 	while (++c < data.cmd_count)
 		ft_check_redirect(data.cmd[c].token);
+	// c = -1;
+	// while (++c < data.cmd_count)
+	// {
+	// 	ft_find_redirect(data, c);
+	// 	ft_clean_token(data, data.cmd[c].token);
+	// }
 }
 
 void 	ft_parse(void)
@@ -513,7 +551,8 @@ void 	ft_parse(void)
 	i = 0;
 	data.cmd_count = ft_token_count(data.buffer, '|');
 	data.cmd = ft_calloc(sizeof(t_cmd), data.cmd_count + 1);
-	if (data.cmd == NULL)
+	data.pid = ft_calloc(sizeof(pid_t), data.cmd_count - 1);
+	if (data.cmd == NULL || data.pid == NULL)
 		ft_exit("Malloc error\n", 2);
 	data.cmd[0].buffer = ft_trim_token(ft_strtok(data.buffer, '|'), ' ');
 	while (++i < data.cmd_count)
@@ -523,55 +562,9 @@ void 	ft_parse(void)
 
 /* **********************ENGINE********************************************** */
 
-char	*ft_get_path(char *buffer)
-{
-	char	*program;
-	char	*env_path;
-	char	**fcnt_path;
-	char 	*test_path;
-	int		i;
-	i = 0;
-	if (access(buffer, F_OK | X_OK) == 0)
-		return (buffer);
-	program = ft_strjoin("/", buffer, 0);
-	env_path = ft_get_variable("PATH");
-	if (env_path == NULL || program == NULL)
-		return (NULL);
-	fcnt_path = ft_split(env_path, ':');
-	if (fcnt_path == NULL)
-		return (NULL);
-	while (fcnt_path[i])
-	{
-		test_path = ft_strjoin(fcnt_path[i], program, 0);
-		if (access(test_path, F_OK | X_OK) == 0)
-			break ;
-		free (test_path);
-		test_path = NULL;
-		i++;
-	}
-	ft_free_array(fcnt_path);
-	free(program);
-	return (test_path);
-}
-
-void	ft_redirect_output_append(t_cmd *cmd)
-{
-	char	*str;
-
-	str = ft_get_next_line(cmd->fd_out);
-	while (str)
-	{
-		if (str)
-			printf("%s", str);
-		free(str);
-		str = ft_get_next_line(cmd->fd_out);
-	}
-	free(str);
-}
-
 bool	ft_execute_builtin(int nb)
 {
-	dprintf(2, "cmd: %s\n", data.cmd[nb].token[0]);
+	//dprintf(2, "cmd: %s\n", data.cmd[nb].token[0]);
 	if (ft_strncmp(data.cmd[nb].token[0], "echo", 4) == 0)
 		ft_echo(data.cmd[nb].token);
 	else if (ft_strncmp(data.cmd[nb].token[0], "env", 3) == 0)
@@ -609,11 +602,11 @@ void	ft_keep_cmd(int nb)
 	
 	data.ct = 1;
 	i = -1;
-	while (data.cmd[nb].token[i++])
+	while (data.cmd[nb].token[++i])
 	{
 		path = NULL;
 		data.cmd[nb].token[0] = data.cmd[nb].token[i];
-		path = ft_get_path(data.cmd[nb].token[0]);
+		path = ft_get_path(nb);
 		if (path || ft_check_builtin(nb, 0) == true)
 		{
 			if (data.cmd[nb].token[1] && (ft_strncmp(data.cmd[nb].token[1], "<", 1) == 0
@@ -637,7 +630,6 @@ void	ft_keep_cmd(int nb)
 	}
 }
 
-
 void	ft_find_redirect(int nb)
 {
 	int	i;
@@ -649,7 +641,7 @@ void	ft_find_redirect(int nb)
 	i = -1;
 	while (data.cmd[nb].token[++i])
 	{
-		dprintf(2, "1\n");
+		//dprintf(2, "1\n");
 		// ft_print_table();
 		if (ft_strncmp(data.cmd[nb].token[i], "<", 1) == 0
 			&& data.cmd[nb].token[i][1] != '<')
@@ -672,10 +664,10 @@ void	ft_find_redirect(int nb)
 			dup2(data.cmd[nb].fd_out, STDOUT_FILENO);
 		}
 	}
-	ft_print_table();
+	//ft_print_table();
 	ft_keep_cmd(nb);
 	ft_clean_token(data.cmd[nb].token);
-	ft_print_table();
+	//ft_print_table();
 }
 
 void	ft_child_suite(int *fd, int nb)
@@ -686,7 +678,7 @@ void	ft_child_suite(int *fd, int nb)
 	if (ft_execute_builtin(nb) == true)
 		ft_exit("", 3);
 	else
-		ft_exec_cmd(ft_get_path(data.cmd[nb].token[0]), nb);
+		ft_exec_cmd(ft_get_path(nb), nb);
 }
 
 void	ft_make_child_process(int nb)
@@ -712,8 +704,7 @@ void	ft_make_child_process(int nb)
 		dup2(fd[0], STDIN_FILENO);
 	}
 }
-
-/* ***********************MAIN*********************************************** */
+/* **********************MAIN************************************************ */
 
 void	ft_fork_main(int nb)
 {
@@ -732,7 +723,7 @@ void	ft_fork_main(int nb)
 		if (ft_execute_builtin(nb) == true)
 			ft_exit("exit fork", 3);
 		else
-			ft_exec_cmd(ft_get_path(data.cmd[nb].token[0]), nb);
+			ft_exec_cmd(ft_get_path(nb), nb);
 		nb = -1;
 		while (++nb < data.cmd_count)
 			waitpid(data.pid[nb], NULL, 0);
